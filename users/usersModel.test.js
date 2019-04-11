bb7f576fde0903731bf4878075ffa6ce3c9af826
brooksvinyard <8202653+brooksvinyard@users.noreply.github.com>
2019-04-11 14:47:58 -0700

    Can remove users. Tests for remove

--- users/usersModel.test.js
+++ users/usersModel.test.js
@@ -26,5 +26,34 @@ describe("users model", () => {
       });
 
     });
+
+    describe("remove()", () => {
+        it("should remove the provided users", async () => {
+            // Add three users...
+            await Users.insert({ name: 'jim' });
+            await Users.insert({ name: 'jam' });
+            await Users.insert({ name: 'jom' });
+            const users = await db('users');
+            expect(users).toHaveLength(3);
+
+            // Remove three users...
+            await Users.remove(1);
+            await Users.remove(2);
+            await Users.remove(3);
+            const delUsers = await db('users');
+            expect(delUsers).toHaveLength(0);
+        });
+  
+        it("should remove the provided user", async () => {
+            await Users.insert({ name: 'jem' });
+            const users = await db('users');
+            expect(users).toHaveLength(1);
+  
+            await Users.remove(1);
+            const delUsers = await db('users');
+            expect(delUsers).toHaveLength(0);
+        });
+  
+      });
   });
   
